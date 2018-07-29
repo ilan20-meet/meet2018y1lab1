805f8f570800d88d6d077d600b2767fd2e0a6cfe
ilan20-meet <ilan20@meet.mit.edu>
2018-07-29 11:13:19 -0400

    Stopping here

---
+++ Avner'sName.py
@@ -0,0 +1,21 @@
+import turtle
+#AVNER
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200+50,-100+200)
+turtle.goto(-200+100,-100)
+turtle.goto(-200+75,-100+100)
+turtle.goto(-200+25,-100+100)
+
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
---
+++ MEETinTurtle.py
@@ -0,0 +1,84 @@
+
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+turtle.goto(-200+200,-100)
+turtle.pendown()
+turtle.goto(-200+300,-100)
+turtle.goto(-200+200,-100)
+turtle.goto(-200+200,-100+200)
+turtle.goto(-200+300,-100+200)
+turtle.goto(-200+200,-100+200)
+turtle.goto(-200+200,-100+100)
+turtle.goto(-200+300,-100+100)
+
+turtle.penup()
+turtle.goto(-200+300,-100)
+turtle.goto(-200+400,-100)
+turtle.pendown()
+turtle.goto(-200+500,-100)
+turtle.goto(-200+400,-100)
+turtle.goto(-200+400,-100+200)
+turtle.goto(-200+500,-100+200)
+turtle.goto(-200+400,-100+200)
+turtle.goto(-200+400,-100+100)
+turtle.goto(-200+500,-100+100)
+
+turtle.penup()
+turtle.goto(-200+500,-100)
+turtle.goto(-200+700,-100)
+turtle.pendown()
+turtle.goto(-200+700,-100+200)
+turtle.goto(-200+600,-100+200)
+turtle.goto(-200+800,-100+200)
+
+#my name
+turtle.penup()
+turtle.goto(-200,-100+200)
+turtle.goto(-200,-100+500)
+turtle.pendown()
+turtle.goto(-200-50,-100+500)
+turtle.goto(-200+50,-100+500)
+turtle.goto(-200,-100+500)
+turtle.goto(-200,-100+300)
+turtle.goto(-200-50,-100+300)
+turtle.goto(-200+50,-100+300)
+
+turtle.penup()
+turtle.goto(-200+150,-100+300)
+turtle.pendown()
+turtle.goto(-200+150,-100+500)
+turtle.goto(-200+150,-100+300)
+turtle.goto(-200+250,-100+300)
+turtle.penup()
+turtle.goto(-200+350,-100+300)
+turtle.pendown()
+turtle.goto(-200+400,-100+500)
+turtle.goto(-200+450,-100+300)
+turtle.goto(-200+425,-100+400)
+turtle.goto(-200+375,-100+400)
+turtle.goto(-200+425,-100+400)
+turtle.goto(-200+450,-100+300)
+turtle.penup()
+turtle.goto(-200+550,-100+300)
+turtle.pendown()
+turtle.goto(-200+550,-100+500)
+turtle.goto(-200+650,-100+300)
+turtle.goto(-200+650,-100+500)
+
+# ...and end it before the next line.
+turtle.mainloop()
+
