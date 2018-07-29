f577e062bb62a674f07c4b81add0ebea06b6805a
ilan20-meet <ilan20@meet.mit.edu>
2018-07-29 09:29:36 -0400

    Stopping here

---
+++ idle3-ilan.py
@@ -0,0 +1,16 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+
+# ...and end it before the next line.
+turtle.mainloop()
+turtle.penup()
